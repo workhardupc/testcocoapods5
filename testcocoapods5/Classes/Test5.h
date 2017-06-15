c36a339f77892af88ad4d2134cef8f49f2de4a56
张中杰 <zhangzhongjie@le.com>
2017-06-15 15:05:41 +0800

    创建测试文件

---
+++ testcocoapods5/Classes/Test5.h
@@ -0,0 +1,13 @@
+//
+//  Test5.h
+//  Pods
+//
+//  Created by bob on 2017/6/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test5 : NSObject
+
+@end
---
+++ testcocoapods5/Classes/Test5.m
@@ -0,0 +1,13 @@
+//
+//  Test5.m
+//  Pods
+//
+//  Created by bob on 2017/6/15.
+//
+//
+
+#import "Test5.h"
+
+@implementation Test5
+
+@end
